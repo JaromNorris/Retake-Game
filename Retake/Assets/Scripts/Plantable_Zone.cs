1f8216251d8e9546f320b0b7716047080b0d1324
Kelley <dragonzaita@gmail.com>
2016-02-17 01:22:45 -0700

    Added plantable zone/space
    Fixed whiteBox to have Plantable spaces in a zone. Also moved it all
    over closer to (0,0,0) for convenience of adding new objects.
    
    TestMushroom and TestSpore were added and replaced in the Player's
    raycast scripting

--- Retake/Assets/Scripts/Plantable_Zone.cs
+++ Retake/Assets/Scripts/Plantable_Zone.cs
@@ -7,5 +7,10 @@ public class Plantable_Zone : MonoBehaviour
 	public int waterPresent;
 	public int pollutionPresent;
 	public Plantable_Space[] spaces;
+
+	void Start()
+	{
+		spaces = GetComponentsInChildren<Plantable_Space>();
+	}
 		
 }
