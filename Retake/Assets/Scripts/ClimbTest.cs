bf7a20d9f7a2dc1eba14311fabf2533f2fc2df42
Luke Alexander <Luketheman@gmail.com>
2016-03-08 19:10:21 -0700

    WaterLilly and Test Ladder
    Water Lillies prefab was added, all 3 versions which should grow into
    each other.  The collision script for the ladder was also added under
    ClimbTest but currently destroys the blocks tagged as "Ladder"

---
+++ Retake/Assets/Scripts/ClimbTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClimbTest : MonoBehaviour
+{
+	void OnTriggerEnter (Collider col)
+	{
+		Debug.Log ("Tada");
+		if(col.gameObject.tag == "Ladder")
+		{
+			Debug.Log("Working");
+			Destroy(col.gameObject);
+		}
+	}
+}
